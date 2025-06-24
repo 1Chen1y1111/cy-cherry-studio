79b40d384b0accbf9208fa1902cf3469fc11526a
1Chen1y1111 <fun20001016@163.com>
2025-06-24 21:49:40 +0800

    fix: auto update

--- src/renderer/src/init.ts
+++ src/renderer/src/init.ts
@@ -6,7 +6,7 @@ function init(): void {
     name: 'CherryAI',
     version: 1.0,
     storeName: 'cherryai',
-    description: 'Cherry Studio storage'
+    description: 'Cherry Studio Storage'
   })
 }
 
