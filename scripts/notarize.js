d81761e11854a4415a892d205c44ac2533651cb1
1Chen1y1111 <fun20001016@163.com>
2025-07-01 16:19:47 +0800

    ci: log notarized app name

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -12,14 +12,15 @@ exports.default = async function notarizing(context) {
   }
 
   const appName = context.packager.appInfo.productFilename
+  const appPath = `${context.appOutDir}/${appName}.app`
 
   await notarize({
-    appPath: `${context.appOutDir}/${appName}.app`,
+    appPath,
     appBundleId: 'com.1Chen1y1111.CyCherryStudio',
     appleId: process.env.APPLE_ID,
     appleIdPassword: process.env.APPLE_APP_SPECIFIC_PASSWORD,
     teamId: process.env.APPLE_TEAM_ID
   })
 
-  console.log('Notarized app:', appName)
+  console.log('  • Notarized app:', appPath)
 }
